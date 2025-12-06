c9ccfefa5b83fa799acedde0cf6f224018507399
small-dogg <sy920112@naver.com>
2025-12-06 16:56:16 +0900

    chore: initial commit

---
+++ src/main/java/com/smalldogg/hospitalsearch/HospitalSearchApplication.java
@@ -0,0 +1,13 @@
+package com.smalldogg.hospitalsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smalldogg/hospitalsearch/HospitalSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smalldogg.hospitalsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
